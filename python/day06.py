993297fca2bcb757b466ee9929e1e47149c1ec6a
jess-spayd <jspayddatasci@gmail.com>
2024-01-16 10:51:59 -0500

    Added case sensitivity

--- python/day06.py
+++ python/day06.py
@@ -2,16 +2,13 @@
 
 def count_char(given_string, given_char):
 
-    given_string_lower = given_string.lower()
-    given_char_lower = given_char.lower()
-
-    char_sum = sum(1 for char in given_string_lower if char == given_char_lower)
+    char_sum = sum(1 for char in given_string if char == given_char)
 
     print('Count of character:', char_sum)
 
 
-given_string = input('Type anything:')
-given_char = input('Select a character to count:')
+given_string = input('Type anything: ')
+given_char = input('Select a character to count (case sensitive): ')
 
 count_char(given_string, given_char)
 
