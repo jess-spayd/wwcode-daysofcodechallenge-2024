c1c77fe7027c182b9f7b48118e0f93a5c3e49845
jess-spayd <jspayddatasci@gmail.com>
2024-01-23 20:27:24 -0500

    add day 14 code for python challenge

---
+++ python/day14.py
@@ -0,0 +1,15 @@
+# Write a program to print the first n numbers of a Fibonacci sequence.
+
+n = int(input('Enter a positive integer: '))
+num1 = 0
+num2 = 1
+next_number = num2  
+count = 1
+ 
+while count <= n:
+    print(next_number, end=" ")
+    count += 1
+    num1, num2 = num2, next_number
+    next_number = num1 + num2
+print()
+
